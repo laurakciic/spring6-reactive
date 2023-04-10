aac3aa14ce2f13adce91fdde6af4bb6b53291a59
Laura <laurakovacic@outlook.com>
2023-04-10 10:14:22 +0200

    get beer by id using path variable

--- src/main/java/com/laurakovacic/spring6reactive/services/BeerService.java
+++ src/main/java/com/laurakovacic/spring6reactive/services/BeerService.java
@@ -2,8 +2,11 @@ package com.laurakovacic.spring6reactive.services;
 
 import com.laurakovacic.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
